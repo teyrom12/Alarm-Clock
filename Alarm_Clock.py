cafd854f20864921cf83b4cbe05c196084fcc7fe
Antonesei Albert <86300499+teyrom12@users.noreply.github.com>
2022-11-10 21:58:38 +0200

    Update Alarm_Clock.py

--- Alarm_Clock.py
+++ Alarm_Clock.py
@@ -1,6 +1,6 @@
 from datetime import datetime   
 from playsound import playsound
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+alarm_time = input("Enter the time of alarm to be set: HH:MM:SS\n")
 alarm_hour=alarm_time[0:2]
 alarm_minute=alarm_time[3:5]
 alarm_seconds=alarm_time[6:8]
@@ -17,11 +17,5 @@ while True:
             print("Good minute")
             if(alarm_seconds==current_seconds):
                 print("Wake Up!")
-                playsound(r'C:\Users\TeyRo\Downloads\PeStancaMeaBas.mpeg')
+                playsound(r'Your\Sound\File\Path\Here.mp3/mpeg')
                 break
-            else: 
-                print("Seconds not yet")
-        else:
-            print(" Minutes not yet")
-    else:
-        print("Hours not yet")
